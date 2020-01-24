29c7e29d3b951feb21915bea43133424e6bd55fe
nano <i20-10@hotmail.fr>
2020-01-24 13:42:24 +0100

    ajout user structure bdd

--- nabeilles.sql
+++ nabeilles.sql
@@ -2,10 +2,10 @@
 -- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Dec 15, 2019 at 12:05 AM
--- Server version: 5.7.27-0ubuntu0.18.04.1
--- PHP Version: 7.2.24-0ubuntu0.18.04.1
+-- Client :  localhost:3306
+-- Généré le :  Ven 24 Janvier 2020 à 13:40
+-- Version du serveur :  5.7.28-0ubuntu0.18.04.4
+-- Version de PHP :  7.2.24-0ubuntu0.18.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Nabeilles`
+-- Base de données :  `nabeilles`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `plant`
+-- Structure de la table `plant`
 --
 
 CREATE TABLE `plant` (
@@ -39,7 +39,7 @@ CREATE TABLE `plant` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `plant`
+-- Contenu de la table `plant`
 --
 
 INSERT INTO `plant` (`id_plant`, `latin_name`, `french_name`, `size`, `definition`, `nectar`, `pollen`, `localisation`, `picture`) VALUES
@@ -48,25 +48,57 @@ INSERT INTO `plant` (`id_plant`, `latin_name`, `french_name`, `size`, `definitio
 (3, 'Alnus glutinosa', 'aulne glutineux', 25, '', 1, 2, 'étang', ''),
 (4, 'Aquilegia vulgaris', 'ancolie', 0.5, '', 2, 3, 'collection historique', '');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `name_user` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname_user` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pseudo` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `name_user`, `firstname_user`, `pseudo`, `level`) VALUES
+(1, 'Jean', 'Aiassez', 'xxAbeillexx', 1);
+
+--
+-- Index pour les tables exportées
 --
 
 --
--- Indexes for table `plant`
+-- Index pour la table `plant`
 --
 ALTER TABLE `plant`
   ADD PRIMARY KEY (`id_plant`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT for table `plant`
+-- AUTO_INCREMENT pour la table `plant`
 --
 ALTER TABLE `plant`
   MODIFY `id_plant` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
